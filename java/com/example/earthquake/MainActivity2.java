34bd5da763530c761dffe2222555cace0ec56948
swapnil135 <68809502+swapnil135@users.noreply.github.com>
2020-11-09 10:01:23 +0530

    Update MainActivity2.java

--- java/com/example/earthquake/MainActivity2.java
+++ java/com/example/earthquake/MainActivity2.java
@@ -2,7 +2,13 @@ package com.example.earthquake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button searchButton =(Button) findViewById(R.id.search_button);
+        final RadioGroup sortButton=findViewById(R.id.sortGroup);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectid=sortButton.getCheckedRadioButtonId();
+                RadioButton selectedButton=findViewById(selectid);
+                TextView numRows=findViewById(R.id.numRows);
+                TextView mag=findViewById(R.id.minMag);
+                MainActivity.minmag=Integer.parseInt(mag.getText().toString());
+                MainActivity.orderby=selectedButton.getText().toString();
+                MainActivity.limit=Integer.parseInt(numRows.getText().toString());
+                Intent i=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
